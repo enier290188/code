d5e9758e63b80d722cbbaa2af8a799c79f83ae25
Enier Ramos Garcia <enier290188@gmail.com>
2024-01-19 12:02:20 -0600

    Updated 4-1-3 Binary Search

--- dsa/4 Recursion/4-1 Illustrative Examples/4-1-3 Binary Search/main.py
+++ dsa/4 Recursion/4-1 Illustrative Examples/4-1-3 Binary Search/main.py
@@ -1,2 +1,23 @@
+def binary_search(data, target, low, high):
+    """
+    Return True if target is found in indicated portion of a Python list.
+    The search only considers the portion from data[low] to data[high] inclusive.
+    """
+    if low > high:
+        return (False,)  # interval is empty; no match
+    else:
+        mid = (low + high) // 2
+        if target == data[mid]:  # found a match
+            return True, mid
+        elif target < data[mid]:
+            # recur on the portion left of the middle
+            return binary_search(data, target, low, mid - 1)
+        else:
+            # recur on the portion right of the middle
+            return binary_search(data, target, mid + 1, high)
+
+
 if __name__ == '__main__':
-    print('PyCharm')
+    __data = [2, 4, 5, 7, 8, 9, 12, 14, 17, 19, 22, 25, 27, 28, 33, 37]
+    __target = 14
+    print(binary_search(data=__data, target=__target, low=0, high=len(__data) - 1))
